59d184c9bf9510504527cb6d84d2142a1ecc9a2a
danilosalys <danilo.pereira.sales@gmail.com>
2024-04-25 17:49:55 -0300

    Fix: Alteracoes no config.json

--- uninstall-service.js
+++ uninstall-service.js
@@ -1,6 +1,6 @@
 let Service = require("node-windows").Service;
 let svc = new Service({
-	name: "Split Files Seven PDV",
+	name: "Mercanet Split Files Seven PDV",
 	description:
 		"Serviço para efetuar a quebra de lotes da SevenPDV para melhoria o processamento no Mercanet!",
 	script: "C:\\projetos-drogacenter\\dc-split-files-sevenpdv\\src\\app.js",
