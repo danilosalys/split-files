0a790ef2c2356857f263e4ae7d3f51b4f0c3caaa
danilosalys <danilo.pereira.sales@gmail.com>
2024-04-25 16:47:51 -0300

    Feature: incluindo o sorter dos arquivos

--- src/controller/SplitFilesController.js
+++ src/controller/SplitFilesController.js
@@ -3,6 +3,7 @@ const SplitFilesModel = require("../model/SplitFilesModel");
 const { logToFile } = require("../utils");
 const INPUT_FOLDER = config.inputFolder;
 const OUTPUT_FOLDER = config.outputFolder;
+const SEND_FOLDERS = config.sendFolders;
 const SPLITED_FILES_FOLDER = config.splitedFilesFolder;
 const RECORD_LOG_Y_N = config.recordLogs;
 const LOG_FOLDER = config.logFolder;
@@ -11,6 +12,7 @@ const LOG_TYPE = config.logType;
 const SplitFilesController = {
 	splitFilesSevenPDV() {
 		try {
+			let index_output_folder = 0;
 			let files = SplitFilesModel.getFiles(INPUT_FOLDER);
 
 			if (!files.length) {
@@ -47,6 +49,22 @@ const SplitFilesController = {
 					INPUT_FOLDER,
 					SPLITED_FILES_FOLDER
 				);
+
+				let splitedFilesToProcess = SplitFilesModel.getFiles(OUTPUT_FOLDER);
+
+				splitedFilesToProcess.forEach((splitedFile) => {
+					index_output_folder > SEND_FOLDERS.length - 1
+						? (index_output_folder = 0)
+						: index_output_folder;
+
+					SplitFilesModel.moveFile(
+						splitedFile.fileName,
+						OUTPUT_FOLDER,
+						SEND_FOLDERS[index_output_folder]
+					);
+					index_output_folder++;
+				});
+
 				RECORD_LOG_Y_N && LOG_TYPE === 1
 					? logToFile(
 							LOG_FOLDER,
